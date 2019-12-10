537572f59980b9b7832b2adde968761c792cc53a
LuisHdezC87 <luis05.hc@gmail.com>
2019-12-09 19:30:44 -0600

    Incomes Module: in progress

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   resources :projects
   resources :contacts
+  resources :incomes
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
